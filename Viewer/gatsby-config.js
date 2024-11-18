a972dd73290397bd238679f7d9dd67f1b9c58eeb
chu ding <cding91@gmail.com>
2024-11-18 15:40:43 -0600

    Use turtle favicon

--- Viewer/gatsby-config.js
+++ Viewer/gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/cfs-database-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/turtles_logo.jpeg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
